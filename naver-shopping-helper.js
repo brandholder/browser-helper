cbdf960f978bc0d85aa01e4eeff31b6acb094d8d
Jaehyun Lee <gisholic@gmail.com>
2021-09-06 18:53:50 +0900

    Update naver-shopping-helper.js

--- naver-shopping-helper.js
+++ naver-shopping-helper.js
@@ -29,6 +29,9 @@ var naverShoppingHelper = {
     var a = document.querySelectorAll('ul[class*="products_seller_list__"] li a[class*="productContent_link_seller__"]');
     for(var b of a){
       console.log(b.getAttribute('data-i') + '\n' + b.querySelector('span[class*="productContent_seller__"]').textContent + '\n' + b.querySelector('div[class*="productContent_info_title__"]').textContent + '\n' + b.querySelector('span[class*="productContent_price__"]').textContent + '\n' + b.href);
+      var span = document.createElement('span');
+      span.innerText = b.getAttribute('data-i');
+      b.closest('div').prepend(span);
     }
   },
 }
