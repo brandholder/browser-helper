cb14583e0bd7f7bd3b5647c7b81d7eb8f4aeac12
Jaehyun Lee <gisholic@gmail.com>
2021-09-06 14:39:30 +0900

    Update naver-shopping-helper.js

--- naver-shopping-helper.js
+++ naver-shopping-helper.js
@@ -1,3 +1,29 @@
+async function scrollPageToBottom(step = 250, delay = 100) {
+  const getScrollHeight = element => {
+    if (!element) return 0;
+    const { scrollHeight, offsetHeight, clientHeight } = element;
+    return Math.max(scrollHeight, offsetHeight, clientHeight);
+  };
+
+  const position = await new Promise(resolve => {
+    let count = 0;
+    const intervalId = setInterval(() => {
+      const { body } = document;
+      const availableScrollHeight = getScrollHeight(body);
+
+      window.scrollBy(0, step);
+      count += step;
+
+      if (count >= availableScrollHeight) {
+        clearInterval(intervalId);
+        resolve(count);
+      }
+    }, delay);
+  });
+
+  return position;
+}
+
 var naverShoppingHelper = {
   getCatalogProductLinks: function() {
     var a = document.querySelectorAll('ul[class*="products_seller_list__"] li a[class*="productContent_link_seller__"]');
